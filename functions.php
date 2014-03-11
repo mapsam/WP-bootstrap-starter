cf599f687d41fd34063903608518830d5e0cd291
svmatthews <matthews.sam@gmail.com>
2014-03-10 17:33:42 -0700

    functions file

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * FUNctions File
+**/
+
+/* Defining the functions directory */
+define("FUN", get_template_directory() . "/functions");
+
+/* Bring those files into this file, which is automatically read by Wordpress */
+require_once FUN . "/wp_bootstrap_navwalker.php";
+require_once FUN . "/general.php";
+
+/* Do not add anything beneath this line! Else the world crumbles */
