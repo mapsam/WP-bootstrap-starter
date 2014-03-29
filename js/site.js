c76eeaf357e6059d2294f12620875facc03b99f0
Sam Matthews <matthews.sam@gmail.com>
2014-03-28 18:10:50 -0700

    closing jquery alias

--- js/site.js
+++ js/site.js
@@ -2,4 +2,4 @@ jQuery(document).ready(function ($) {
 	// This prep function does two things
 	// 1. Loads all of your javascript once the document is ready
 	// 2. Recreates the "$" alias for use because WP automatically loads jQuery in noConflict() mode
-}
+});
